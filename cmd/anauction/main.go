0861ee3fd03129f5f7457ba3da3a65c911138eb7
indigowar <a10levsion@gmail.com>
2024-06-12 13:12:17 +0300

    updated main.go

--- cmd/anauction/main.go
+++ cmd/anauction/main.go
@@ -2,26 +2,65 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
 
+	"github.com/alexedwards/scs/v2"
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 
+	"github.com/indigowar/anauction/domain/service"
 	"github.com/indigowar/anauction/handlers"
+	"github.com/indigowar/anauction/storage/postgres"
 )
 
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	router := echo.New()
 
+	if err := godotenv.Load(); err != nil {
+		logger.Error("Failed to load env file", "Err", err)
+		os.Exit(1)
+	}
+
+	dbConn, err := connectToDB()
+	if err != nil {
+		logger.Error("Failed to connect to database", "Err", err)
+		os.Exit(1)
+	}
+
+	userStorage := postgres.NewUserStorage(dbConn)
+
+	authService := service.NewAuth(logger, userStorage)
+
+	sm := scs.New()
+	sm.Lifetime = 24 * time.Hour
+
+	router := echo.New()
 	handlers.Setup(router, handlers.SetupSettings{
-		Logger: logger,
+		Logger:         logger,
+		SessionManager: sm,
+		Auth:           &authService,
 	})
 
 	run(router, logger)
+}
+
+func connectToDB() (*pgx.Conn, error) {
+	// urlExample := "postgres://username:password@localhost:5432/database_name"
+	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_DB"),
+	)
+
+	return pgx.Connect(context.Background(), url)
 }
 
 func run(router *echo.Echo, logger *slog.Logger) {
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/labstack/echo/v4 v4.12.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
