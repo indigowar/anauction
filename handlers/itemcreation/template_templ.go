e5ada91a2a3da5bb14397c377ce4179270c7ee9d
indigowar <a10levsion@gmail.com>
2024-06-27 17:08:04 +0300

    made simple calendar that requires some future fixes

--- handlers/itemcreation/template_templ.go
+++ handlers/itemcreation/template_templ.go
@@ -138,6 +138,14 @@ func createForm(handle string) templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(" ")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			templ_7745c5c3_Err = form.Calendar(form.CalendarOpts{}).Render(ctx, templ_7745c5c3_Buffer)
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
 			if !templ_7745c5c3_IsBuffer {
 				_, templ_7745c5c3_Err = io.Copy(templ_7745c5c3_W, templ_7745c5c3_Buffer)
 			}
